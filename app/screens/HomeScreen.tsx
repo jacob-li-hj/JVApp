0f78aaca8ea7cf8f9fc8e5c5d57991f7957a5ba6
jacob-li-hj <jacobli@aaxischina.com>
2021-08-17 20:52:11 +0800

    add touch-id

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
+import TouchID from 'react-native-touch-id';
 import { Box } from '@jv/ui-storybook-mobile-app/basic';
 import useDemo from '@framework/demo/use-demo';
 import { useUI } from '@ui-hook';
 
 const HomeScreen: React.FC<any> = ({ navigation }) => {
 	const { data } = useDemo();
-	const [uiState] = useUI<[string, Function]>('uiState');
+	const [uiState] = useUI('uiState');
+
+	const checkTouchId = () => {
+		TouchID.isSupported()
+			.then((biometryType) => {
+				console.log(`biometryType is: ${biometryType}`);
+				return TouchID.authenticate().then((success) => {
+					console.log(success);
+				});
+			})
+			.catch((error) => {
+				console.log(`touch or face ID error: ${error}`);
+			});
+	};
+
 	return (
 		<View style={styles.content}>
 			<Text>{data?.hello}</Text>
 			<Text>{uiState}</Text>
+			<Button onPress={checkTouchId} title={`TouchID`} />
 			<Button onPress={() => navigation.navigate('Notifications')} title={`Go to notifications`} />
 			<Box
 				bg="primary.400"
